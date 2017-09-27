2242ef4496bbbc2ea27056cc051811abc5609488
TI Cortex M4 <ti.cortex.m4@gmail.com>
2017-09-27 17:42:15 +0300

    format HTML

--- demo-web-server-energia/EthernetWebserver/make.bat
+++ demo-web-server-energia/EthernetWebserver/make.bat
@@ -1,6 +1,14 @@
 @echo off
 setLocal EnableDelayedExpansion
 
-for /f "tokens=* delims= " %%a in (1.html) do (
-echo   client.println(^"%%a^"^); >> 2.html
+if exist index.c del /F index.c
+
+for /f "tokens=* delims= " %%a in (index.html) do (
+echo   client.println(^"%%a^"^); >> index.c
+)
+
+if exist config.c del /F config.c
+
+for /f "tokens=* delims= " %%a in (config.html) do (
+echo   client.println(^"%%a^"^); >> config.c
 )
